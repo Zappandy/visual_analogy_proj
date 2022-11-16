a7cc8a40c8d4fe0a77440f8271195d98e31adee8
Andres <nanoandres_24@hotmail.com>
2022-11-16 12:40:35 +0100

    inheritance between cutter and resizer

--- data_loaders.py
+++ data_loaders.py
@@ -5,7 +5,7 @@ import os
 from torch.utils.data import Dataset, DataLoader
 from torch.nn.utils.rnn import pad_sequence
 
-class ImageDataset(Dataset):
+class ResizeImageDataset(Dataset):
 
     def __init__(self, image_dir, pattern=".jpg"):
         """Initialize the attributes of the object of the class."""
@@ -28,6 +28,43 @@ class ImageDataset(Dataset):
 
         return  [f.path for f in os.scandir(directory) if f.path.endswith(pattern)]  # ends with does not like regex
 
+class CutterImageDataset(ResizeImageDataset):
+
+    def __init__(self):
+        super.__init__()
+        self.cutter = ImageCutter
+        self.create_cut_batch(self.image_path_list)
+
+    def create_cut_batch(self, index):
+        for path in self.image_path_list:
+
+            path = self.image_path_list[index]
+            im = cv2.imread(file_path)
+            height, width = im.shape[:2]
+            image_cut_vals = self.cutter(width, height)
+            corr_width = image_cut_vals.corr_width
+            corr_height = image_cut_vals.corr_height
+        self.tile_generator()
+
+    def tile_generator(self, width=width, height=height, corr_width, corr_height=corr_height):
+        all_tiles = list()
+        for y in range(0, height, corr_height):  # 800, 200
+            for x in range(0, width, corr_width):  # 526, 263
+                y1 = y + corr_height
+                x1 = x + corr_width
+                tiles = image_copy[y:y+corr_height, x:x+corr_width]
+                # for viz comment for now
+                #cv2.imwrite('tile'+str(y1)+'_'+str(x1)+'.jpg', tiles)
+                #cv2.rectangle(image, (x, y), (x1, y1), (0, 255, 0), 1)
+                all_tiles.append(tiles)
+
+        # for viz comment for now
+        #plt.imshow(image[:, :, ::-1])
+        #plt.axis('off')
+        #cv2.imwrite("patched.jpg",image)
+
+        return all_tiles
+
 class Rescale:
 
     def __init__(self, heights, widths, scaler_method=min):
@@ -40,6 +77,26 @@ class Rescale:
         scalar method dependent on init
         """
         return cv2.resize(image, self.points, interpolation=cv2.INTER_LINEAR)
+
+class ImageCutter:
+
+    def __init__(self, width, height):
+        self.corr_width = self.crop_values(width)
+        self.corr_height = self.crop_values(height)
+
+    def crop_values(self, measure):
+        divisors = [n for n in range(2, 10) if measure%n == 0]  # we don't want insane amount of crops
+        if len(divisors) == 1:
+            result = divisors[0]    
+        elif not divisors:
+            result = measure
+        else:
+            if len(divisors) % 2 != 0:
+                result = int(np.median(divisors))
+            else:
+                idx = len(divisors) // 2
+                result = divisors[idx-1]  # upper end == idx    
+        return measure // result
 
 
 def visual_collate_fn(batch, scaler_method, naive_approach=False):
